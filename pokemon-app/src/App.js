f4576e74bc02326528500f8f792c41bd740ec0a6
Ana Caroline Sinigaglia <anacarolinesinigaglia@gmail.com>
2020-11-01 17:49:31 -0300

    CSS e responsividade com useState

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks'; //{ } significa que só vou chamar pelo seu nome
 import { PokemonsContainer } from './containers/PokemonsContainer';
+import './App.css';
 
 function App() {
   const client = new ApolloClient({ //instanciei ApolloClient na variável client
--- pokemon-app/src/containers/PokemonsContainer.js
+++ pokemon-app/src/containers/PokemonsContainer.js
@@ -1,20 +1,31 @@
 //AQUI FAREI MEU LOOP
 
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_POKEMONS } from "../graphql/get-pokemons";
 import { Pokemon } from "../components/Pokemon";
 
 export function PokemonsContainer() {
-//podemos retornar os dados e os erros tb através do graphQL, nesse caso retornamos o dado de 'pokemons'
+  const [quantity, setQuantity] = useState(0);
+  //podemos retornar os dados e os erros tb através do graphQL, nesse caso retornamos o dado de 'pokemons'
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     //data contem um array de pokemons, cujo valor default é vazio
-    variables: { first: 9 }, //ao realizarmos uma query, o resultado voltará no array "pokemons"
+    variables: { first: quantity }, //ao realizarmos uma query, o resultado voltará no array "pokemons"
   });
 
   return (
-    <div className="pokemons">
-    {/* se "pokemon" existir, faz essa função que converte o valor recebido para uma string JSON:
+    <div className="wrapper">
+      <div className="container-one">
+        <p>How many Pokemons?</p>
+        <input
+          type="number"
+          onChange={(event) => setQuantity(event.target.value)}
+          className="input-quantity"
+        />
+      </div>
+
+      <div className="container-two">
+        {/* se "pokemon" existir, faz essa função que converte o valor recebido para uma string JSON:
       {pokemons && pokemons.map(pokemon => JSON.stringify(pokemon, null, 2))} 
       é a mesma coisa que: 
         if(pokemons) {
@@ -22,7 +33,11 @@ export function PokemonsContainer() {
                 JSON.stringify(pokemon)
             }) 
     NEVERMIND, NO PRÓXIMO PASSO SUBSTITUÍMOS O JSON.stringify por um componente, veja:*/}
-        {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        {pokemons &&
+          pokemons.map((pokemon) => (
+            <Pokemon key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </div>
     </div>
   );
 }
