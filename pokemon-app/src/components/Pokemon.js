5ff4804114adcfaa80157d9da777405196318781
Ana Caroline Sinigaglia <anacarolinesinigaglia@gmail.com>
2020-11-01 15:47:59 -0300

    Pokemons showing up

--- pokemon-app/src/components/Pokemon.js
+++ pokemon-app/src/components/Pokemon.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
 
 export function Pokemon({ pokemon }) {
-    return (
-        <p>Hello!</p>
-    )
+  return (
+    <div className="pokemon">
+
+      <div className="pokemon-name">
+        <p>{pokemon.name}</p>
+      </div>
+
+      <div className="pokemon-meta">
+        <span>{pokemon.maxHP}</span>
+        <span>{pokemon.maxCP}</span>
+      </div>
+
+      <div className="pokemon-image">
+          <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+
+      <div className="pokemon-attacks">
+          {pokemon 
+          && pokemon.attacks 
+          && pokemon.attacks.special 
+          .slice(0, 3).map(attack => (
+              <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span> //uso outra key pq o id já estamos usando em outro lugar
+          ))}
+      </div>
+
+    </div>
+  );
 }
